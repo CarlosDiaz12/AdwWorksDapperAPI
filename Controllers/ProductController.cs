e0baf4b7953185c133c070efd94c490e70df8ab9
Carlos Diaz <carlos.cedp@gmail.com>
2022-06-30 10:54:08 -0400

    added dapper and one query implemented

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using AdwWorksDapperAPI.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdwWorksDapperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _productRepository.GetProducts();
+            return Ok(result);
+        }
+    }
+}
--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -1,14 +1,21 @@
 ﻿using AdwWorksDapperAPI.Infrastructure;
 using AdwWorksDapperAPI.Interfaces;
 using AdwWorksDapperAPI.Models;
+using Dapper;
 using Microsoft.Extensions.Options;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace AdwWorksDapperAPI.Repositories
 {
     public class ProductRepository : IProductRepository
     {
         private readonly AdwWorksConfig _adwWorksConfig;
+        private IDbConnection Connection
+        {
+            get { return new SqlConnection(_adwWorksConfig.ConnectionString); }
+        }
         public ProductRepository(IOptions<AdwWorksConfig> adwWorksConfig)
         {
             _adwWorksConfig = adwWorksConfig.Value;
@@ -30,7 +37,9 @@ namespace AdwWorksDapperAPI.Repositories
 
         public IEnumerable<Product> GetProducts()
         {
-            throw new System.NotImplementedException();
+            using IDbConnection dbConnection = Connection;
+            dbConnection.Open();
+            return dbConnection.Query<Product>("SELECT top 10 * FROM [SalesLT].[Product]");
         }
 
         public bool ProductExists(int productId)
